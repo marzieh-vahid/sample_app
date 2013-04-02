7dde4fa9bbd2ff49eb724dff330a30a19041ee59
marzieh.vahid <marzieh.vahid@gmail.com>
2013-04-02 20:46:48 +0430

    Add user microposts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe Micropost do
 
   let(:user) { FactoryGirl.create(:user) }
-  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
+
+  before do
+    @micropost = user.microposts.build(content: "Lorem ipsum")
+  end
 
   subject { @micropost }
 
@@ -17,16 +20,17 @@ describe Micropost do
   describe "accessible attributes" do
     it "should not allow access to user_id" do
       expect do
-        Micropost.new(user_id: user.id)
-      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-    end    
+        Micropost.new(user_id: "1")
+      end.should raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
   end
 
   describe "when user_id is not present" do
     before { @micropost.user_id = nil }
     it { should_not be_valid }
   end
-    describe "with blank content" do
+
+  describe "with blank content" do
     before { @micropost.content = " " }
     it { should_not be_valid }
   end
