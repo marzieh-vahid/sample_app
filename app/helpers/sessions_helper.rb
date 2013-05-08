ea0f085c2b73771d3328432b85291e5d77b68114
marzieh.vahid <marzieh.vahid@gmail.com>
2013-05-08 14:51:25 +0430

    The End

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,13 +2,13 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    self.current_user = user
   end
 
   def signed_in?
     !current_user.nil?
   end
-
+  
   def current_user=(user)
     @current_user = user
   end
@@ -21,24 +21,24 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
   def sign_out
-    current_user = nil
+    self.current_user = nil
     cookies.delete(:remember_token)
   end
-
-  def store_location
-    session[:return_to] = request.fullpath
-  end
-
+  
   def redirect_back_or(default)
     redirect_to(session[:return_to] || default)
     session.delete(:return_to)
   end
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please sign in."
-    end
+  def store_location
+    session[:return_to] = request.url
   end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'e24437bc1523d9323d7180bc8fb3eff44d1cb5263ccf68ec1f7721e7347cef04a3eef3373cc8f69eb8dba5495617deec31b5e742381ff930471aec734876aa0b'
+SampleApp::Application.config.secret_token = '05a4ebbcfb455a58acdcffde8ddb93dd1c98db230a6bcf894040bee0d3eabee7c0c5a19e250b4daa3c21ff34124459e9871242a91d5592113a00904da8f15c91'
